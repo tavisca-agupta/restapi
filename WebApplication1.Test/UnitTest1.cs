72d952724657a41237bdd2205e67375248c4db68
agupta <agupta@tavisca.in>
2019-09-05 23:44:36 +0530

    v2

--- WebApplication1.Test/UnitTest1.cs
+++ WebApplication1.Test/UnitTest1.cs
@@ -1,14 +1,20 @@
 using System;
 using Xunit;
+using WebApplication1.DataBase;
+using WebApplication1.Model;
+using Newtonsoft.Json;
 
 namespace WebApplication1.Test
 {
     public class UnitTest1
     {
+        BookServices BS = new BookServices();
         [Fact]
         public void Test1()
         {
-
+            Book b = new Book{BookId= 2,BookName= "Never Ending",Category= "Romance",Price= 500,Author= "Chetan Bhagat" };
+            string JSONobject = JsonConvert.SerializeObject(b);
+            BS.AddBook(JSONobject);
         }
     }
 }
